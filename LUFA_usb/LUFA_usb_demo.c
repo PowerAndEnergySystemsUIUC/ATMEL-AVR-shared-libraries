33db52436212b189bbd15bc478bd3d1edbfff256
Josiah McClurg <jcmcclurg@gmail.com>
2013-04-02 13:23:04 -0600

    Initial commit.
    I apologize for not forking from original LUFA source, but I'm short on
    time, and this can be done in the future if it is felt to be necessary.

---
+++ LUFA_usb/LUFA_usb_demo.c
@@ -0,0 +1,19 @@
+/*
+ * LUFA_usb_demo.c
+ *
+ * Created: 2/2/2012 9:02:40 PM
+ *  Author: Josiah McClurg
+ */ 
+
+#ifdef LUFA_USB_DEMO
+
+void main(void)
+{
+	while(1)
+	{
+		
+	}
+	return;
+}
+
+#endif
